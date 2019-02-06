6071c37e587071e2e63bf085e215b0cfe3be93dd
Rachel Moskowitz <rachelbmoskowitz@gmail.com>
2019-02-06 11:31:23 -0500

    deployment fixes

--- sequelize.js
+++ sequelize.js
@@ -1,17 +1,7 @@
 const Sequelize = require('sequelize');
 const TweetModel = require("./models/tweet.js")
-const sequelize = new Sequelize("process.env.DATABASE_URL || 'postgres://localhost:5432/twitter", {
-  dialect: 'postgres',
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-
-  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
-  operatorsAliases: false
+const sequelize = new Sequelize("process.env.DATABASE_URL || 'postgres://localhost:5432/twitter'", 'rachelmoskowitz', null, {
+  dialect: 'postgres'
 });
 
 const Tweet = TweetModel(sequelize, Sequelize)

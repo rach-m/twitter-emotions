99fce96864b7c89e5700c65904c93f5088aee77c
Rachel Moskowitz <rachelbmoskowitz@gmail.com>
2019-02-10 18:27:45 -0500

    fixes

--- models/sequelize.js
+++ models/sequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL,{
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:3000/twitter', {
   dialect: 'postgres'
 });
 
